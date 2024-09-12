6890693c04ae1a8a350a4d93a4a9400ef4e2262b
Piyabud <piyabud.dev@gmail.com>
2024-09-12 21:42:54 +0700

    feat : Security banner

--- src/components/homepages/BodyComponent.vue
+++ src/components/homepages/BodyComponent.vue
@@ -1,11 +1,13 @@
 <template>
     <Customer />
+    <Security />
     <IsoCertified />
 </template>
 
 <script setup>
 import Customer from './body/Customer.vue';
 import IsoCertified from './body/IsoCertified.vue';
+import Security from './body/Security.vue';
 
 
 </script>
