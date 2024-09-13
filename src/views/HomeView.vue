99e18152f0c4af3914e7f3481e07e3fa1cdf85ea
Piyabud <piyabud.dev@gmail.com>
2024-09-13 16:56:20 +0700

    feat : footer paper count

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,14 @@
+
+<template>
+        <HeaderComponent />
+        <BodyComponent />
+        <FooterComponent />
+</template>
+
 <script setup>
 import HeaderComponent from '@/components/homepages/HeaderComponent.vue';
 import BodyComponent from '@/components/homepages/BodyComponent.vue';
 import FooterComponent from '@/components/homepages/FooterComponent.vue';
 </script>
-<template>
-        <HeaderComponent/>
-        <BodyComponent />
-        <FooterComponent />
-</template>
 
-<style>
-
-</style>
+<style></style>
